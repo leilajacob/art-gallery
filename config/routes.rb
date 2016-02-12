90d7602a7b8ac110a0dd598f407bc1631941e829
Leila Jacob <leilajacob@me.com>
2016-02-12 14:57:57 -0500

    changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   resources :paintings
-  root 'welcome#index'
+  root 'paintings#index'
+  get 'home' => 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
