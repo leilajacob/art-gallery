731bab2d2db992e386bc816795dae1ef1ef8c8c7
Leila Jacob <leilajacob@me.com>
2016-02-12 15:09:19 -0500

    design

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
 
 	def index
+		@paintings = Painting.all
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   resources :paintings
-  root 'paintings#index'
-  get 'home' => 'welcome#index'
+  
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
