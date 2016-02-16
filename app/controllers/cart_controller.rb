1514c6d638ff8c4e5ac19dfc689362326081381d
Leila Jacob <leilajacob@me.com>
2016-02-16 16:00:38 -0500

    styles

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -3,14 +3,29 @@ class CartController < ApplicationController
 	before_filter  :authenticate_user!, :except => [:add_to_cart, :view_order]
 
   def add_to_cart
-  	line_item = LineItem.new
-  	line_iten.painting_id = params[:painting_id]
-  	line_item.save
+  	painting = Painting.find(params[:painting_id])
+    if painting.quantity < params[:qty].to_i
+      redirect_to painting, notice: "Not enough quantity in stock."
+    else
 
-  	line_item.line_item_total = line_item.painting.price
-  	line_item.save
+      line_item = LineItem.new
+      line_item.painting_id = params[:painting_id].to_i
+      line_item.quantity = params[:qty]
 
-  	redirect_to root_path
+    if user_signed_in?
+      line_item.customer_key = current_user.id
+    else
+      line_item.customer_key = remote_ip
+    end 
+
+    line_item.save
+
+    line_item.line_item_total = line_item.quantity * line_item.painting.price
+    line_item.save 
+
+    redirect_to view_order_path
+
+    end 
   end
 
   def view_order
